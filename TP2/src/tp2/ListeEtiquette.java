0c18af9ba692e67249d009db209972fc9f5d4c44
flavoiewmvs <flavoie@wmvs.ca>
2016-06-16 11:28:43 -0400

    test

--- TP2/src/tp2/ListeEtiquette.java
+++ TP2/src/tp2/ListeEtiquette.java
@@ -13,8 +13,8 @@ public class ListeEtiquette<E> extends ArrayList<E> implements Iterable<E> {
     }
 
     public void ajouterEtiquette(Etiquette a_etiquette) {
-//          Cette méthode va permettre d’ajouter une étiquette à la structure. Il n’y a pas de règle pour ces
-//          étiquettes. Elles seront considéré comme valide, peu importe leurs contenu.
+//Cette méthode va permettre d’ajouter une étiquette à la structure. Il n’y a pas de règle pour ces
+//étiquettes. Elles seront considéré comme valide, peu importe leurs contenu.
     }
 
     public void supprimerEtiquette(Etiquette a_etiquette) {
@@ -28,14 +28,26 @@ public class ListeEtiquette<E> extends ArrayList<E> implements Iterable<E> {
 //sont supprimées.
     }
 
-    public void iterator(int a_position) {
-        ;//Iterator< Etiquette>
+    public Iterator<Etiquette> iterator(int a_position) {
 //Cette méthode retourne un itérateur sur des étiquettes. L’utilisateur donne la position d’une case
 //du tableau (indice). L’itérateur va permettre de parcourir toutes les étiquettes qui contiennent cette
 //case dans leur intervalle.
+
+        int pos = 0;
+
+  
+
+    public boolean hasNext() {
+        return pos < ListeEtiquette.this.size();
     }
 
-    public Iterator<E> iterator(String a_id) {
+    public Integer next() {
+        return (Integer) ListeEtiquette.this.get(pos++);
+    }
+
+}
+
+public Iterator<E> iterator(String a_id) {
 //         Iterator< E>
 //Finalement, cette méthode va retourner un iterateur qui parcours les cases du tableau d’éléments.
 //Par contre, seule les cases qui font partie d’un intervalle ayant l’id indiqué seront parcourues.
@@ -43,50 +55,6 @@ public class ListeEtiquette<E> extends ArrayList<E> implements Iterable<E> {
 //Les cases doivent être parcourues dans le même ordre que nous les retrouvons dans le tableau.
 //Iterator iter = this.iterator();
 
-   Iterator<E> it = new Iterator<E>() {
-
-            private int currentIndex = 0;
-
-            @Override
-            public boolean hasNext() {
-                return currentIndex < currentSize && this[currentIndex] != null;
-            }
-
-            @Override
-            public E next() {
-                return this[currentIndex++];
-            }
-
-            @Override
-            public void remove() {
-                throw new UnsupportedOperationException();
-            }
-        };
-        return it;
-//        return new iterListe(this);
-//        return this.iterator();
     }
-
-//    public class iterListe<S> implements Iterator<S> {
-//    public iterListe(ListeEtiquette<S> a_liste) {
-//        maListe = a_liste;
-//    }
-//
-//        Iterator<S> it = new Iterator<S>() {
-//
-//            private int currentIndex = 0;
-//
-//            @Override
-//            public boolean hasNext() {
-//                return currentIndex < currentSize && arrayList[currentIndex] != null;
-//            }
-//
-//            @Override
-//            public Type next() {
-//                return arrayList[currentIndex++];
-//            }
-//        }
-//    };
-//    return it ;
 
 }
